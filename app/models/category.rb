f463911cdb6cd3b7d39e6ec3c76f309cb86032f3
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-24 17:40:00 +0900

    修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
-class Category < Active_Hash::Base
-  self.date = [
+class Category < ActiveHash::Base
+  self.data = [
    { id: 1, name: '---' },
    { id: 2, name: 'メンズ' },
    { id: 3, name: 'レディース' },
--- app/models/deli_time.rb
+++ app/models/deli_time.rb
@@ -1,5 +1,5 @@
-class Deli_time < Active_Hash::Base
-  self.date = [
+class Deli_time < ActiveHash::Base
+  self.data = [
    { id: 1, name: '---' },
    { id: 2, name: '1~2日で発送' },
    { id: 3, name: '2~3日で発送' },
