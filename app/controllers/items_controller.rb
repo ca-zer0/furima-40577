2f3ccd8c0900ad0d5e7172ade28d57d9590f2adb
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-24 17:24:18 +0900

    itemsコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
 
@@ -11,13 +13,14 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
+      puts @item.errors.full_messages
       render :new, status: :unprocessable_entity
     end
   end
 
   def item_params
     params.require(:item).permit(
-      :name,:describe, :price,:Category_id, :condition_id, 
+      :name,:describe, :price,:category_id, :condition_id, 
       :deli_charge_id, :prefecture_id, :deli_time_id, :image
       ).merge(user_id: current_user.id)
   end
