5ef352c47c496652ade937e5f9089d44c60a511d
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-24 17:10:46 +0900

    Reapply "itemモデル作成、マイグレーション記述"
    This reverts commit d7037d76b77ecd28dd9d92358f89606b4c97b5d0.

---
+++ app/models/item.rb
@@ -0,0 +1,22 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :Category
+
+  with_options presence: true do
+    validates :name
+    validates :describe
+    validates :price, format: { with: /\A[0-9]+\z/}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+    validates :user
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
+    validates :condition_id, numericality: { other_than: 1, message: "can't be blank"} 
+    validates :deli_charge_id, numericality: { other_than: 1, message: "can't be blank"} 
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"} 
+    validates :deli_time_id, numericality: { other_than: 1, message: "can't be blank"} 
+    validates :image, attached_file_presence: true
+  end
+
+
+  belongs_to :user 
+  has_one_attached :image
+  has_one :order
+end
