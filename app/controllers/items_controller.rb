f37689e94a4e76cf2d981ef07a86d5da76399035
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-24 17:10:52 +0900

    Reapply "ストロングパラメータ定義"
    This reverts commit 127a79d485bca14e8496b41d658098a0ecc03f7d.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def item_params
+    params.require(:item).permit(
+      :name,:describe, :price,:Category_id, :condition_id, 
+      :deli_charge_id, :prefecture_id, :deli_time_id, :image
+      ).merge(user_id: current_user.id)
+  end
 end
