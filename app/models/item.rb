8291f9629086c6f55e44e9ada65f85c1a934060c
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-24 17:16:47 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
     validates :deli_charge_id, numericality: { other_than: 1, message: "can't be blank"} 
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"} 
     validates :deli_time_id, numericality: { other_than: 1, message: "can't be blank"} 
-    validates :image, attached_file_presence: true
+    validates :image
   end
 
 
