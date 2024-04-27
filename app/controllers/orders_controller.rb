e63d9fc991d73f8514c36fd8c2fefaa94df96fdb
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-27 21:51:14 +0900

    切り離しに応じモデル名など変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,32 +2,35 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.new
+    @order_form = OrderForm.new
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
-       pay_item
-       @order.save
-       return redirect_to root_path
+  @item = Item.find(params[:item_id])
+  @order_form = OrderForm.new(order_params)
+  if @order_form.valid?
+    pay_item(order_params)
+    if @order_form.save
+      redirect_to root_path
     else
-       @item = Item.find(params[:item_id])
-       render :index, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
+  else
+    render :index, status: :unprocessable_entity
   end
+end
 
   private
     def order_params
-    params.require(:order).permit(:post_code, :prefecture_id, :city, :street, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id,token: params[:token])
+    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :street, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id,token: params[:token])
     end
 
-    def pay_item
-    Payjp.api_key = "sk_test_49c11788ac378b9c903c6032"
+    def pay_item(order_params)
+      Payjp.api_key = "sk_test_49c11788ac378b9c903c6032"
       Payjp::Charge.create(
-        amount: order_params[:price],
+        amount: @item.price,
         card: order_params[:token],
         currency: 'jpy'
       )
-    end
+  end
 end
