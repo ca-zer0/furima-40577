5b63d83581decec25b632572b1fc0adb234238a1
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-28 02:45:16 +0900

    order_formモデル修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,14 +1,14 @@
-class OrderForm
+class OrderForm < ApplicationRecord
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :city, :street, :building, :phone_number, :token
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "can't be blank. is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :street
-    validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "can't be blank. is invalid"}
     validates :token
   end
   def save(params,user_id)
