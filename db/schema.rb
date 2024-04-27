853b4f1e95da89338cd413752ee7b6f8b97ffdae
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-28 02:44:08 +0900

    意味なさげなorder_formテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_27_090048) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_27_164919) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -67,6 +67,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_27_090048) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "order_forms", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_forms_on_item_id"
+    t.index ["user_id"], name: "index_order_forms_on_user_id"
+  end
+
   create_table "orders", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
@@ -98,6 +107,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_27_090048) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "orders"
   add_foreign_key "items", "users"
+  add_foreign_key "order_forms", "items"
+  add_foreign_key "order_forms", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
 end
