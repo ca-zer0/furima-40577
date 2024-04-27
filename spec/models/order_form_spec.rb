8a8afeb428959f0627ec5d8f65f159a7f13f8c01
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-28 03:14:28 +0900

    エラー文分岐

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe OrderForm, type: :model do
     it 'post_codeは『３桁ハイフン４桁』半角英数字でないと保存できないこと' do
       @order_form.post_code = '123-123４'
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include("Post code can't be blank. is invalid. Include hyphen(-)")
+      expect(@order_form.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
     end
     it 'prefectureに「---」が選択されている場合は購入できないこと' do
       @order_form.prefecture_id = '1'
@@ -52,17 +52,17 @@ RSpec.describe OrderForm, type: :model do
     it 'phone_numberが9桁以下だと購入できないこと' do
       @order_form.phone_number = '090123456'
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include("Phone number can't be blank. is invalid")
+      expect(@order_form.errors.full_messages).to include("Phone number is invalid")
     end
     it '電話番号が12桁以上だと購入できない' do
       @order_form.phone_number = '090123456789'
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include("Phone number can't be blank. is invalid")
+      expect(@order_form.errors.full_messages).to include("Phone number is invalid")
     end
     it 'phone_numberが半角数値でないと購入できないこと' do
       @order_form.phone_number = '０9012341234'
       @order_form.valid?
-      expect(@order_form.errors.full_messages).to include("Phone number can't be blank. is invalid")
+      expect(@order_form.errors.full_messages).to include("Phone number is invalid")
     end
     it 'tokenが空では購入できないこと' do
       @order_form.token = nil
