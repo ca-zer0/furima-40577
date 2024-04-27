b58a977d0e362c0827419f06125b404de8e3e5d7
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-27 20:27:57 +0900

    不要なファイル削除、モデル名など変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,22 +2,23 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @orderform = OrderForm.new
+    @order = Order.new
   end
 
   def create
-    @orderform = OrderForm.new(order_params)
-    if @orderform.valid?
-       @orderform.save(params,current_user.id)
-       redirect_to root_path
+    binding.pry
+    @order = Order.new(order_params)
+    if @order.valid?
+       @order.save
+       return redirect_to root_path
     else
        @item = Item.find(params[:item_id])
-       render :index
+       render :index, status: :unprocessable_entity
     end
   end
 
   private
     def order_params
-    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :street, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:order).permit(:post_code, :prefecture_id, :city, :street, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
     end
 end
