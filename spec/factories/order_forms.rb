076ba86eca133f50565ae76ccb2d670cdc9f94c1
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-28 02:44:36 +0900

    order_formのfactorybot定義

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :order_form do
-    item_id {1}
-    user_id {4}
-    post_code {Faker::Number.leading_zero_number(digits: 4)}
-    prefecture_id
+    post_code {Faker::Number.leading_zero_number(digits: 3) + '-' + Faker::Number.leading_zero_number(digits: 4)}
+    prefecture_id {4}
     city {Faker::Address.city}
     street {Faker::Address.street_address}
     phone_number {Faker::Number.leading_zero_number(digits: 11)}
     token {"tok_abcdefghijk00000000000000000"}
+
+    association :user
+    association :item
   end
 end
