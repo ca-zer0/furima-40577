1503c6b4e3b92590d6e389d4b3a9ea06b8934c97
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-27 20:26:56 +0900

    特定の要素があるページでのみ読み込むよう編集

--- app/javascript/commission_calc.js
+++ app/javascript/commission_calc.js
@@ -1,11 +1,14 @@
 window.addEventListener('turbo:load', () => {
-  const price = document.getElementById("item-price")
-  
-  price.addEventListener('input', function(){
-    const inputValue = price.value;
-    const commission = document.getElementById("add-tax-price")
-    commission.innerHTML = Math.floor(inputValue * 0.1 )
-    const profit = document.getElementById("profit")
-    profit.innerHTML = Math.floor(inputValue - Number(commission.innerHTML))
-  });
+  const price = document.getElementById("item-price");
+
+  if(price){
+    price.addEventListener('input', function(){
+      const inputValue = price.value;
+      const addTaxPrice = document.getElementById("add-tax-price");
+      addTaxPrice.innerHTML = Math.floor(inputValue * 0.1);
+
+      const profit = document.getElementById("profit");
+      profit.innerHTML = Math.floor(inputValue - Number(addTaxPrice.innerHTML));
+    });
+  }
 });
