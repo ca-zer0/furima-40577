63582c52dcc4e84658ebf618981d17782732c1d9
ca-zer0 <kokorogamochimochi@gmail.com>
2024-04-28 17:07:26 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :users, only: [:new, :create]
+  # resources :users, only: [:new, :create]
   resources :items do
     resources :orders, only: [:index, :create]
   end
